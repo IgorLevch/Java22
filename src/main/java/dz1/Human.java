c235f06b558c463a5f0de22260878468db12a54d
IgorLevch <igorlevvv@gmail.com>
2022-04-22 18:41:02 +0300

    Initial commit

--- src/main/java/dz1/Human.java
+++ src/main/java/dz1/Human.java
@@ -2,6 +2,22 @@ package dz1;
 
 public class Human implements Mooving{
 
+    private int pheight;
+    private int plength;
+
+    public Human(int pheight, int plength) {
+        this.pheight = pheight;
+        this.plength = plength;
+    }
+
+    public int getPheight() {
+        return pheight;
+    }
+
+    public int getPlength() {
+        return plength;
+    }
+
     @Override
     public void jump() {
         System.out.println("Human can jump");
